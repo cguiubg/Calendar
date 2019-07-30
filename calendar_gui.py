d62e1ace8c3bf86d485516a78bb4a07c93e44496
John <johandrews953@gmail.com>
2019-07-30 16:56:52 -0400

    commenting and fixing postioning

--- calendar_gui.py
+++ calendar_gui.py
@@ -2,20 +2,25 @@ import tkinter as tk
 import math
 import json
 
-LOWHEIGHT = 0.97
-ENTRYHEIGHT = 0.030
+#delcaring heights entry widgets
+LOWEST_Y = 0.97 #relative y in window for the lowest entry widget
+ENTRY_HEIGHT = 0.03 
+ENTRY_Y = 0.03 #relative height of entry widgets
 
-with open('settings.json') as f:
+with open('settings.json') as f: #opening the settings.json file
     settings = json.load(f)
 
-def process_command(event=None):
+#commands are passed here and then sent to the needed function
+def process_command(event=None) -> None: 
     input_ = entry.get()
-    move_command_up(input_)
+    move_history(input_, settings['indicator'])
 
-def move_command_up(string: str):
-    temp_r1 = string
-    temp_l1 = settings['indicator']
-    for side, history in text_list:
+#takes a string for log a string for the side bar, moves all strings up the history
+def move_history(text: str, side: str) -> None:
+    temp_r1 = text
+    temp_l1 = side
+    #iterating through history of text log and moving the text and side to the next
+    for side, history in text_log:
         side.config(state=tk.NORMAL)
         history.config(state=tk.NORMAL)
         temp_l2 = side.get()
@@ -32,50 +37,64 @@ def move_command_up(string: str):
 
 root = tk.Tk()
 
+#binding the processes command to enter
+root.bind('<Return>', func=process_command)
+
+#creating the canvas
 canvas = tk.Canvas(root, height=settings['window']['height'], width=settings['window']['width'])
 canvas.pack()
 
+#getting the font tuple from the settings.json
 font = (settings['font']['type'], settings['font']['size'])
 
+#making the frame where input is displayed
 input_frame = tk.Frame(root, bg='#252525')
 input_frame.config()
 input_frame.place(relx=0, rely=0, relwidth=0.3, relheight=1)
 
-root.bind('<Return>', func=process_command)
-
+#making the sidebar and the main input widget
 sidebar = tk.Entry(input_frame)
 entry = tk.Entry(input_frame)
 sidebar.config(readonlybackground='#1C1C1C', fg='#A3f774', font=font, bd=0, highlightthickness=0)
 sidebar.insert(0, settings['indicator'])
 sidebar.config(state='readonly')
 entry.config(bg='#1C1C1C', fg='#A3f774', font=font, bd=0, highlightthickness=0)
+#if invert is true in settings, history goes top (most recent) to bottom (oldest)
 if settings['invert']:
-    sidebar.place(relx=0, rely=0, width=25, relheight=1-LOWHEIGHT)
-    entry.place(x=25, rely=0, relwidth=0.90, relheight=1-LOWHEIGHT)
+    sidebar.place(relx=0, rely=0, width=25, relheight=1-LOWEST_Y)
+    entry.place(x=25, rely=0, relwidth=0.95, relheight=1-LOWEST_Y)
+#otherwise, history goes bottom (most recent) to top (oldest)
 else:
-    sidebar.place(relx=0, rely=LOWHEIGHT, width=25, relheight=1-LOWHEIGHT)
-    entry.place(x=25, rely=LOWHEIGHT, relwidth=0.90, relheight=1-LOWHEIGHT)
+    sidebar.place(relx=0, rely=LOWEST_Y, width=25, relheight=1-LOWEST_Y)
+    entry.place(x=25, rely=LOWEST_Y, relwidth=0.95, relheight=1-LOWEST_Y)
 
-entry_num = int(math.ceil(LOWHEIGHT/ENTRYHEIGHT))
+#finding the number of entry widgets necesarry for the history log
+entry_num = int(math.ceil(LOWEST_Y/ENTRY_HEIGHT))
 
-text_list = [(tk.Entry(input_frame), tk.Entry(input_frame)) for _ in range(entry_num)]
+#making the list of entry widgets in tuples for the history log
+text_log = [(tk.Entry(input_frame), tk.Entry(input_frame)) for _ in range(entry_num)]
 i = 1
-for side, history in text_list:
+#configuring each entry widget in the log and placing them
+for side, history in text_log:
     side.config(readonlybackground='#2D2D2D', fg='#F7C974', font=font, bd=0, highlightthickness=0)
     side.config(state='readonly')
     history.config(readonlybackground='#252525', fg='#E4F774', font=font, bd=0, highlightthickness=0)
     history.config(state='readonly')
     if settings['invert']:
-        side.place(relx=0, rely=i*ENTRYHEIGHT, width=25, relheight=ENTRYHEIGHT)
-        history.place(x=25, rely=i*ENTRYHEIGHT, relwidth=0.90, relheight=ENTRYHEIGHT)
+        side.place(relx=0, rely=i*ENTRY_Y, width=25, relheight=ENTRY_HEIGHT)
+        history.place(x=25, rely=i*ENTRY_Y, relwidth=0.90, relheight=ENTRY_HEIGHT)
     else:
-        side.place(relx=0, rely=LOWHEIGHT-i*ENTRYHEIGHT, width=25, relheight=ENTRYHEIGHT)
-        history.place(x=25, rely=LOWHEIGHT-i*ENTRYHEIGHT, relwidth=0.90, relheight=ENTRYHEIGHT)
+        side.place(relx=0, rely=LOWEST_Y-i*ENTRY_Y, width=25, relheight=ENTRY_HEIGHT)
+        history.place(x=25, rely=LOWEST_Y-i*ENTRY_Y, relwidth=0.90, relheight=ENTRY_HEIGHT)
     i += 1
 
+#making the frame for the calendar display
 displayFrame = tk.Frame(root, bg='#1C1C1C')
 displayFrame.place(relx=0.3, rely=0, relwidth=0.7, relheight=1)
 
-# root.resizable(height=False, width=False)
+#setting root details
 root.title('Calendar')
+root.minsize(500, 500)
+
+#mainloop
 root.mainloop()
