0edfdb1bb5ec979d4205875db8ddb3924ad5a6ae
John <johandrews953@gmail.com>
2019-07-30 15:59:10 -0400

    fixed location for sidebar

--- calendar_gui.py
+++ calendar_gui.py
@@ -15,17 +15,17 @@ def process_command(event=None):
 def move_command_up(string: str):
     temp_r1 = string
     temp_l1 = settings['indicator']
-    for text_l, text_r in text_list:
-        text_l.config(state=tk.NORMAL)
-        text_r.config(state=tk.NORMAL)
-        temp_l2 = text_l.get()
-        temp_r2 = text_r.get()
-        text_l.delete(0, tk.END)
-        text_r.delete(0, tk.END)
-        text_l.insert(0, temp_l1)
-        text_r.insert(0, temp_r1)
-        text_l.config(state='readonly')
-        text_r.config(state='readonly')
+    for side, history in text_list:
+        side.config(state=tk.NORMAL)
+        history.config(state=tk.NORMAL)
+        temp_l2 = side.get()
+        temp_r2 = history.get()
+        side.delete(0, tk.END)
+        history.delete(0, tk.END)
+        side.insert(0, temp_l1)
+        history.insert(0, temp_r1)
+        side.config(state='readonly')
+        history.config(state='readonly')
         temp_l1 = temp_l2
         temp_r1 = temp_r2
     entry.delete(0, tk.END)
@@ -43,34 +43,34 @@ input_frame.place(relx=0, rely=0, relwidth=0.3, relheight=1)
 
 root.bind('<Return>', func=process_command)
 
-entry_aux = tk.Entry(input_frame)
+sidebar = tk.Entry(input_frame)
 entry = tk.Entry(input_frame)
-entry_aux.config(readonlybackground='#1C1C1C', fg='#A3f774', font=font, bd=0, highlightthickness=0)
-entry_aux.insert(0, settings['indicator'])
-entry_aux.config(state='readonly')
+sidebar.config(readonlybackground='#1C1C1C', fg='#A3f774', font=font, bd=0, highlightthickness=0)
+sidebar.insert(0, settings['indicator'])
+sidebar.config(state='readonly')
 entry.config(bg='#1C1C1C', fg='#A3f774', font=font, bd=0, highlightthickness=0)
 if settings['invert']:
-    entry_aux.place(relx=0, rely=0, relwidth=0.10, relheight=1-LOWHEIGHT)
-    entry.place(relx=0.10, rely=0, relwidth=0.90, relheight=1-LOWHEIGHT)
+    sidebar.place(relx=0, rely=0, width=25, relheight=1-LOWHEIGHT)
+    entry.place(x=25, rely=0, relwidth=0.90, relheight=1-LOWHEIGHT)
 else:
-    entry_aux.place(relx=0, rely=LOWHEIGHT, relwidth=0.10, relheight=1-LOWHEIGHT)
-    entry.place(relx=0.10, rely=LOWHEIGHT, relwidth=0.90, relheight=1-LOWHEIGHT)
+    sidebar.place(relx=0, rely=LOWHEIGHT, width=25, relheight=1-LOWHEIGHT)
+    entry.place(x=25, rely=LOWHEIGHT, relwidth=0.90, relheight=1-LOWHEIGHT)
 
 entry_num = int(math.ceil(LOWHEIGHT/ENTRYHEIGHT))
 
 text_list = [(tk.Entry(input_frame), tk.Entry(input_frame)) for _ in range(entry_num)]
 i = 1
-for text_l, text_r in text_list:
-    text_l.config(readonlybackground='#2D2D2D', fg='#F7C974', font=font, bd=0, highlightthickness=0)
-    text_l.config(state='readonly')
-    text_r.config(readonlybackground='#252525', fg='#E4F774', font=font, bd=0, highlightthickness=0)
-    text_r.config(state='readonly')
+for side, history in text_list:
+    side.config(readonlybackground='#2D2D2D', fg='#F7C974', font=font, bd=0, highlightthickness=0)
+    side.config(state='readonly')
+    history.config(readonlybackground='#252525', fg='#E4F774', font=font, bd=0, highlightthickness=0)
+    history.config(state='readonly')
     if settings['invert']:
-        text_r.place(relx=0.1, rely=i*ENTRYHEIGHT, relwidth=0.90, relheight=ENTRYHEIGHT)
-        text_l.place(relx=0, rely=i*ENTRYHEIGHT, relwidth=0.10, relheight=ENTRYHEIGHT)
+        side.place(relx=0, rely=i*ENTRYHEIGHT, width=25, relheight=ENTRYHEIGHT)
+        history.place(x=25, rely=i*ENTRYHEIGHT, relwidth=0.90, relheight=ENTRYHEIGHT)
     else:
-        text_r.place(relx=0.1, rely=LOWHEIGHT-i*ENTRYHEIGHT, relwidth=0.90, relheight=ENTRYHEIGHT)
-        text_l.place(relx=0, rely=LOWHEIGHT-i*ENTRYHEIGHT, relwidth=0.10, relheight=ENTRYHEIGHT)
+        side.place(relx=0, rely=LOWHEIGHT-i*ENTRYHEIGHT, width=25, relheight=ENTRYHEIGHT)
+        history.place(x=25, rely=LOWHEIGHT-i*ENTRYHEIGHT, relwidth=0.90, relheight=ENTRYHEIGHT)
     i += 1
 
 displayFrame = tk.Frame(root, bg='#1C1C1C')
