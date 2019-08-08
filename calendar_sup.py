89abf5d23bc2b63c5a6d30a2fe2758cfe2bdbc26
John <johandrews953@gmail.com>
2019-08-07 23:12:25 -0400

    made suplement file; outlined TODO

---
+++ calendar_sup.py
@@ -0,0 +1,14 @@
+#TODO implement following methods:
+
+def add_event():
+    return
+
+def add_event_recursive():
+    return
+
+def remove_event():
+    return
+
+def remove_event_recursive():
+    return
+
